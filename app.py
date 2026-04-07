26f0b5f8641cd10e4745c05cd58412fabb05c95f
Colin <colin@shier.ca>
2026-04-06 23:05:28 -0400

    Add strike zone visual

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from src.data_loader import load_data
 from src.preprocess import clean_data
+import matplotlib.patches as patches
 import matplotlib.pyplot as plt
 
 st.title("Guelph Royals Scouting Dashboard")
@@ -13,7 +14,23 @@ pitcher = st.selectbox("Select Pitcher", df['pitcher_name'].unique())
 filtered_df = df[df['pitcher_name'] == pitcher]
 
 # 3. Simple EDA Visualization
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(5,6))
+
+strike_zone = patches.Rectangle((-0.85, 1.5), 1.7, 2.0, 
+                                linewidth=2, edgecolor='black', 
+                                facecolor='none', linestyle='--')
+
+ax.add_patch(strike_zone)
+
+
+
 ax.scatter(filtered_df['x'], filtered_df['y'], c='blue', alpha=0.5)
+
+ax.set_xlim(-2,2)
+ax.set_ylim(0,5)
+ax.set_aspect('equal')
 ax.set_title(f"Pitch Locations for {pitcher}")
+ax.set_xlabel("Horizontal Location(ft)")
+ax.set_ylabel("Vertical Height(ft)")
+
 st.pyplot(fig)

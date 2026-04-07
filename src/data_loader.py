e5e0642239f4cfab7632b72fa7a4b8fc8d6b2df8
Colin <colin@shier.ca>
2026-04-06 21:30:10 -0400

    Functional data loader from google sheets

---
+++ src/data_loader.py
@@ -0,0 +1,29 @@
+import os
+import pandas as pd
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SHEET_ID = os.getenv("GOOGLE_SHEETS_ID")
+API_KEY = os.getenv("GOOGLE_SHEETS_API")
+
+def load_data(sheet_name="Sheet1"):
+    url = f"https://sheets.googleapis.com/v4/spreadsheets/{SHEET_ID}/values/{sheet_name}?key={API_KEY}"
+
+    response = requests.get(url)
+    data = response.json()
+
+    rows=data.get("values",[])
+
+    if not rows:
+        raise ValueError("No data found in sheet.")
+    
+    df = pd.DataFrame(rows[1:],columns=rows[0])
+
+    return df
+
+
+if __name__ == "__main__":
+    df = load_data()
+    print(df.head())
